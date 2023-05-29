6b0f24f0c32a986da3cf9911ec4f8676da167c8b
Elena <knopka-el@ya.ru>
2023-05-30 01:45:06 +0200

    fix the bug

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-let nameInput = document.getElementById("name-input").value;
-console.log(nameInput);
-let avatarInput = document.getElementById("avatar-input").value;
-console.log(avatarInput);
-let commentInput = document.getElementById("comment-input").value;
-console.log(commentInput);
+
+const nameInput = document.getElementById('name-input');
+const avatarInput = document.getElementById('avatar-input');
+const commentInput = document.getElementById('comment-input');
+const resultLink = document.getElementById('result-link');
+const resultName = document.getElementById('result-name');
+const resultComment = document.getElementById('result-comment');
 
 
-function sendName(){
-    nameInput = document.getElementById("name-input").value;
-    nameInput = nameInput.replaceAll('', '');
-    console.log(nameInput);
-    let firstLetter = nameInput[0].toUpperCase();
-    let name = nameInput.slice(1).toLowerCase();
-    nameInput =  firstLetter + name;
-    console.log(nameInput);
-    nameInput = document.getElementById("result-name").innerText ;
+function transformName(name) {
+  name = name.trim();
+  name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  return name;
 }
 
-function sendAll(){
-document.getElementById("result-link").src = avatarInput;
-sendName();
-sendComment();
-
+function sendAll() {
+  const name = nameInput.value;
+  const avatar = avatarInput.value;
+  const comment = commentInput.value;
+  const transformedName = transformName(name);
+  const transformedComment = checkSpam(comment);
+  resultLink.src = avatar;
+  resultName.innerText = transformedName;
+  resultComment.innerText = transformedComment;
+  const result = document.querySelector('.result');
 }
-function checkSpam(){
 
+function checkSpam(str) {
+  const lowerCaseStr = str.toLowerCase();
+  const replacedStr = lowerCaseStr.replace('viagra', '***').replace('xxx', '***');
+  return replacedStr;
 }
+
+
+
+
 
